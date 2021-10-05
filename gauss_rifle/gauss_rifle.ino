2e37d7badc638709560296b03c188234dbf0dab2
david parker <17parker@gmail.com>
2021-10-04 21:43:31 -0700

    Well I wrote the whole program again. This time it only took like an hour

--- gauss_rifle/gauss_rifle.ino
+++ gauss_rifle/gauss_rifle.ino
@@ -53,11 +53,73 @@ WHAT I NEED:
 //Finding out setup() and loop() were just part of the Arduino library made me uncomfortable...
 //An empty sketch is only like 150 bytes what the hell
 
+#define GATE_PIN PIN_9 //PB1 - PWM OC1A
+#define LED1 PIN_6 //PD6 - PWM OC0A
+#define SENSOR1 PIN_A0 //PC0
+
+#define LED1_DUTY 120 //0-255, duty cycle of LED PWM
+
+#define TRIGGER_VAL 76
+#define RESET_VAL 80
+volatile uint8_t fired = 0;
+
+#define GATE_COMP_VAL 120
+#define GATE_PRESCALER CLK_256
+
+void stop_timer1() { TCCR1B &= 0b11111000; }
+void start_timer1() { TCCR1B |= GATE_PRESCALER; }
+
+ISR(TIMER1_COMPA_vect) {
+	stop_timer1();
+	fired = 1;
+	//toggle_pinB(PIN_13);
+}
+
+ISR(ADC_vect) {
+	//adc_read_8_bit();
+	if (ADCH <= TRIGGER_VAL) {
+		if (fired)
+			return;
+		start_timer1();
+	}
+	else if (ADCH >= RESET_VAL) {
+		fired = 0;
+	}
+}
+
 int main() {
+	//Serial.begin(115200);
+	cli();
 	init_ports();
-	//Enter setup code here
+	//built in led
+	//disable_pullupB(PIN_13);
+	//set_outputB(PIN_13);
 
+	disable_pullupB(GATE_PIN);
+	disable_pullupD(LED1);
+	disable_pullupC(SENSOR1);
+	set_outputB(GATE_PIN);
+	set_outputD(LED1);
+	//Setup the sensor's LED
+	set_timer0(WGM_FAST_TOP, CLK_1, 0b10, LED1_DUTY);
 
+	//Setup the sensor's ADC
+	adc_select_pin(ADC0);
+	adc_disable_interrupt();
+	adc_enable_adc();
+	adc_set_prescaler(ADC_PRESCALER_8);
+	adc_set_free_run_trigger_source();
+	adc_enable_auto_trigger();
+	adc_init();
+
+	//Setup the gate's timer
+	set_timer1(FAST_8_BIT, CLK_OFF, 0b10, GATE_COMP_VAL);
+
+	timer1_interrupt_masks(0b00000010);
+	adc_enable_interrupt();
+	sei();
 	//I'm pretty sure the gauss rifle ran only within interrupts...
-	while (1) {}
+	while (1) {
+		//Serial.println(adc_read_value);
+	}
 }

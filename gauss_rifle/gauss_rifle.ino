fb68f77260d27025a72a1cc6fa02d67fd97c2c21
17parker <17parker@gmail.com>
2022-08-16 02:17:39 -0700

    Update gauss_rifle.ino

--- gauss_rifle/gauss_rifle.ino
+++ gauss_rifle/gauss_rifle.ino
@@ -1,9 +1,9 @@
 #include "ATMEGA328p.h"
 #include "ATMEGA328p_TIMERS.h"
 
-#define GATE_PIN PIN_9  //GATE PIN - PB1 - PWM OC1A
+#define GATE_PIN PIN_9  	//GATE PIN - PB1 - PWM OC1A
 #define LED1 PIN_6		//LED1 PIN - PD6 - PWM OC0A
-#define SENSOR1 PIN_A0  //PC0
+#define SENSOR1 PIN_A0 		//PC0
 
 #define LED1_DUTY 120 //0-255, duty cycle of LED PWM
 
@@ -22,7 +22,7 @@ ISR(TIMER1_COMPA_vect) {
 	I'm using a timer (basically PWM)
 	The PWM starts HIGH, when it goes LOW the interrupt is triggered
 	*/
-	stop_timer1();	//Stops the timer so there is only 1 pulse
+	stop_timer1();		//Stops the timer so there is only 1 pulse
 	fired = 1;		//Flag, otherwise it'll keep re-firing
 }
 
@@ -40,14 +40,9 @@ ISR(ADC_vect) {
 }
 
 int main() {
-	/***
-	* This block, until the line "while(1)" is essentially "void setup()"
-	***/
+	cli();		
+	init_ports();
 
-	cli();		//Disable global interrupts
-	init_ports();	//Set all pins as INPUT_PULLUP
-
-	//Self explanatory
 	disable_pullupB(GATE_PIN);
 	disable_pullupD(LED1);
 	disable_pullupC(SENSOR1);
@@ -58,17 +53,17 @@ int main() {
 	set_timer0(WGM_FAST_TOP, CLK_1, 0b10, LED1_DUTY);	//A lot happens in this function
 
 	//Enable the ADC input on ADC0
-	ADMUX &= 0b11110000;	//Clear current MUX3:0
-	ADMUX |= ADC0;			//Write new MUX3:0
+	ADMUX &= 0b11110000;	
+	ADMUX |= ADC0;
 
-	ADCSRA &= ~(1 << 3);	//Disable the ADC interrupt during setup
-	ADCSRA |= 0b10000000;	//Enable the ADC
+	ADCSRA &= ~(1 << 3);	
+	ADCSRA |= 0b10000000;	
 	adc_set_prescaler(ADC_PRESCALER_8);
 	ADCSRB &= 0b11111000;	//Set ADC free run mode (trigger new analog read right after it finishes)
-	ADCSRA |= (1 << 5);	//Enable ADC auto trigger (When user-specified event happens, trigger analog read)
+	ADCSRA |= (1 << 5);	//Enable ADC auto trigger (new ADC conversion starts right after one finishes)
 	
 	//This sets the reference voltage for the ADC to Vcc, selected ADC pin to ADC0
-	//There needs to be a capacitor from AREF to Vcc if the reference value is Vcc
+	//There needs to be a capacitor from AREF to Vcc
 	//The read value is left-adjusted (ADLAR = 1)
 	ADMUX = (1 << 5) | (1 << 6);
 	ADCSRA |= (1 << 6);		//Starts the reading, first read takes 25 cycles vs 13
@@ -82,8 +77,7 @@ int main() {
 	
 	sei(); 	//Enable global interrupts
 
-	//This is essentially "void loop()"
 	while (1) {
-		//It's all in interrupts, so nothing happens here
+		//It's all in interrupts
 	}
 }
